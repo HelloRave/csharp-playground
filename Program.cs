8dccc8d27df2c1f420a9c745a0ac0c1678025f4a
weiwei.huang <weiwei.huang@brtchip.com>
2023-09-25 15:19:57 +0800

    Escape character

--- Program.cs
+++ Program.cs
@@ -6,23 +6,16 @@
 
     {
 
-        static string firstName = "Hello";
-        static string lastName = "World";
-        static string fullName = string.Concat(" ", firstName, lastName, " ");
-        static string formatName = String.Format("My name is {0}", fullName);
+        static string escapedString = "\"Hello World\" \\ \na line break";
 
         public static void Main(string[] args)
 
         {
             
-            Console.WriteLine(fullName); // output: " Hello World "
-            Console.WriteLine(firstName.Substring(2)); // output: "llo"
-            Console.WriteLine(firstName.ToLower()); // output: "hello"
-            Console.WriteLine(firstName.ToUpper()); // output: "HELLO"
-            Console.WriteLine(fullName.Trim()); // output: "Hello World"
-            Console.WriteLine(firstName.IndexOf('e')); // output: 1
-            Console.WriteLine(String.IsNullOrWhiteSpace(firstName)); // output: false
-            Console.WriteLine(formatName);
+            Console.WriteLine(escapedString); 
+            /** output:
+             "Hello World" \
+             a line break */
 
         }
 
