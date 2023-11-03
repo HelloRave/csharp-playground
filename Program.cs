3affd0f524e812452abddda3fa2b318a864e4e65
weiwei.huang <weiwei.huang@brtchip.com>
2023-11-03 11:40:28 +0800

    Math class

--- Program.cs
+++ Program.cs
@@ -4,20 +4,19 @@ using System.Diagnostics.Metrics;
 namespace DataTypesVariables
 
 {
-    enum Day { Mo, Tu, We, Th, Fr = 6, Sa, Su };
     public class Program
 
     {
         public static void Main(string[] args)
         {
-            Day day = Day.Mo;
-            Console.WriteLine(day);
-            Console.WriteLine((int)day);
-
-            foreach (var eachDay in Enum.GetValues(typeof(Day)))
-            {
-                Console.WriteLine(eachDay);
-            }
+            Console.WriteLine("Ceiling: {0}", Math.Ceiling(15.3));
+            Console.WriteLine("Floor: {0}", Math.Floor(15.3));
+            Console.WriteLine("Min: {0}", Math.Min(15.3, 16));
+            Console.WriteLine("Max: {0}", Math.Max(15.3, 16));
+            Console.WriteLine("3^2: {0}", Math.Pow(3, 2));
+            Console.WriteLine("Pi: {0}", Math.PI);
+            Console.WriteLine("Square root of 25: {0}", Math.Sqrt(25));
+            Console.WriteLine("Absolute: {0}", Math.Abs(-25));
         }
     }
 
