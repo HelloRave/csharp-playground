7f5858e45df384ac3162574ce8dabb9f14c450f7
weiwei.huang <weiwei.huang@brtchip.com>
2023-11-09 18:15:39 +0800

    Delegates

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using csharp_playground;
+using System.Collections;
 using System.Diagnostics.Metrics;
 using System.Text.RegularExpressions;
 
@@ -8,17 +9,67 @@ namespace DataTypesVariables
     public class Program
 
     {
+        internal delegate bool FilterDelegate(Person person);
         public static void Main(string[] args)
         {
-            if (args.Length == 0)
+            // Example 1
+            List<string> names = new List<string>() { "Aiden", "Sif", "Walter", "Anatoli" };
+
+            Console.WriteLine("----before----");
+            foreach (string name in names)
             {
-                Console.WriteLine("Please provide args");
-                Console.ReadKey();
-                return;
+                Console.WriteLine(name);
             }
-            Console.WriteLine(args[0]);
-            Console.ReadKey();
+
+            names.RemoveAll(Filter);
+
+            Console.WriteLine("----after----");
+
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+            // Example 2
+            List<Person> values = new List<Person>()
+            {
+                new Person() { Name = "Aiden", Age = 41 },
+                new Person() { Name = "Sif", Age = 69 },
+                new Person() { Name = "Walter", Age = 12 },
+                new Person() { Name = "Anatoli", Age = 25 },
+            };
+
+            DisplayPeople("Kids", values, IsMinor);
+            DisplayPeople("Adults", values, IsAdult);
         }
+        public static bool Filter(string s)
+        {
+            return s.Contains('i');
+        }
+
+        internal static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+        {
+            Console.WriteLine(title);
+            foreach (Person person in people)
+            {
+                if(filter(person))
+                {
+                    Console.WriteLine("{0}, {1} years old", person.Name, person.Age);
+                }
+            }
+        }
+
+        internal static bool IsMinor(Person person)
+        {
+            return person.Age < 18;
+        }
+
+        internal static bool IsAdult(Person person)
+        {
+            return person.Age > 18;
+        }
+
+
     }
 
 }
