0ab7187a61df603b1e01ca6283aecb405d913342
weiwei.huang <weiwei.huang@brtchip.com>
2023-11-09 17:32:03 +0800

    Main Args

--- Program.cs
+++ Program.cs
@@ -10,23 +10,14 @@ namespace DataTypesVariables
     {
         public static void Main(string[] args)
         {
-            DateTime dateTime = new DateTime(2018, 5, 31);
-
-            Console.WriteLine("Date is {0}", dateTime);
-            Console.WriteLine("Today: {0}", DateTime.Today);
-            Console.WriteLine("Time now: {0}", DateTime.Now);
-            Console.WriteLine("Minutes of time now: {0}", DateTime.Now.Minute);
-            Console.WriteLine("Tomorrow: {0}", DateTime.Now.AddDays(1));
-
-            Console.WriteLine("Write date in yyyy-mm-dd");
-            string input = Console.ReadLine();
-            DateTime output;
-            if (DateTime.TryParse(input, out output))
+            if (args.Length == 0)
             {
-                Console.WriteLine(output);
-                TimeSpan daysPassed = DateTime.Now.Subtract(output);
-                Console.WriteLine("Days passed: {0}", daysPassed.Days);
+                Console.WriteLine("Please provide args");
+                Console.ReadKey();
+                return;
             }
+            Console.WriteLine(args[0]);
+            Console.ReadKey();
         }
     }
 
